aad313f61ab9db556bffb05705174cff63a2afa9
Jeff Dolatshahi <jeff.dolatshahi@gmail.com>
2016-08-09 10:56:42 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 #################################################################
-#
-# Remember:
+# makeCacheMatrix() creates a special "matrix" object that
+# can cache its inverse.
 #
 # The <<- operator is used to assign a value to an object in an
 # environment that is different from the current environment.
@@ -11,8 +11,7 @@
 #
 #################################################################
 #
-# makeCacheMatrix() creates a special "matrix" object that
-# can cache its inverse.
+# makeCacheMatrix(x)
 #
 #################################################################
 makeCacheMatrix <- function(x) = matrix()) {
@@ -27,22 +26,4 @@ makeCacheMatrix <- function(x) = matrix()) {
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
-}
-#################################################################
-#
-# cacheSolve() calculates the inverse of the special "matrix"
-# returned by makeCacheMatrix(). If the inverse has already
-# been calculated, and the matrix has not changed, then
-# cachSolve() will retrieve the inverse from the cache.
-#
-#################################################################
-cacheSolve <- function(x, ...) {
-    m <- x$getsolve()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setsolve(m)
 }
